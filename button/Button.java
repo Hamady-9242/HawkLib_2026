bd39ab4f65a77db7da15a13e34048e7e72f87b03
techno-thor <joejeakle@gmail.com>
2026-03-10 17:22:23 -0400

    Mostly xbox controller tweaks

--- button/Button.java
+++ button/Button.java
@@ -12,30 +12,52 @@ public class Button {
 
     private final Supplier<Boolean> TRIGGER;
 
+    /**
+     * 
+     * @param trigger
+     */
     public Button(Supplier<Boolean> trigger) { 
         TRIGGER = trigger;
         ButtonManager.add(this); 
     }
 
+    /**
+     * 
+     * @return
+     */
     public boolean get() { return TRIGGER.get(); }
 
+    /**
+     * 
+     * @return
+     */
     public boolean getPressed() {
         boolean tempPressed = mPressed;
         mPressed = false;
         return tempPressed;
     }
 
+    /**
+     * 
+     * @return
+     */
     public boolean getReleased() {
         boolean tempReleased = mReleased;
         mReleased = false;
         return tempReleased;
     }
 
+    /**
+     * 
+     */
     public void clear() {
         mPressed = false;
         mReleased = false;
     }
 
+    /**
+     * 
+     */
     public void update() {
         boolean currentValue = get();
 
--- hid/XboxController.java
+++ hid/XboxController.java
@@ -4,23 +4,41 @@ import edu.wpi.first.math.MathUtil;
 
 public class XboxController extends edu.wpi.first.wpilibj.XboxController {
     private double mAxisDeadzoneThreshold = 0.10;
-    private double mTriggerButtonThreshold = 0.50;
+    private double mTriggerButtonThreshold = 0.90;
 
+    private boolean mXAxisInverted = false;
     private boolean mYAxisInverted = false;
     
     public XboxController(int port) { super(port); }
 
-	public void configAxisDeadzone(double value) { mAxisDeadzoneThreshold = value; }
-	public void configTriggerThreshold(double value) { mTriggerButtonThreshold = value; }
+	public XboxController configAxisDeadzone(double value) { 
+        mAxisDeadzoneThreshold = value; 
+        return this;
+    }
+	public XboxController configTriggerThreshold(double value) { 
+        mTriggerButtonThreshold = value; 
+        return this;
+    }
 
-	public void configYAxisInverted(boolean isInverted) { mYAxisInverted = isInverted; }
+	public XboxController configXAxisInverted(boolean isInverted) { 
+        mXAxisInverted = isInverted; 
+        return this;
+    }
+
+	public XboxController configYAxisInverted(boolean isInverted) { 
+        mYAxisInverted = isInverted; 
+        return this;
+    }
 
     @Override public double getRawAxis(int axis) { return MathUtil.applyDeadband(super.getRawAxis(axis), mAxisDeadzoneThreshold); }
+
+    @Override public double getLeftX() { return mXAxisInverted ? -super.getLeftX() : super.getLeftX(); }
+    @Override public double getRightX() { return mXAxisInverted ? -super.getRightX() : super.getRightX(); }
 
     @Override public double getLeftY() { return mYAxisInverted ? -super.getLeftY() : super.getLeftY(); }
     @Override public double getRightY() { return mYAxisInverted ? -super.getRightY() : super.getRightY(); }
 
-	public boolean getLeftTrigger() { return getLeftTriggerAxis() >= mTriggerButtonThreshold; }
-	public boolean getRightTrigger() { return getRightTriggerAxis() >= mTriggerButtonThreshold; }
+	public boolean getLeftTriggerButton() { return getLeftTriggerAxis() >= mTriggerButtonThreshold; }
+	public boolean getRightTriggerButton() { return getRightTriggerAxis() >= mTriggerButtonThreshold; }
 	public double getTriggerAxis() { return getRightTriggerAxis() - getLeftTriggerAxis(); }
 }
