eddf18ae47404f2035a49b7c22dc8ed3651c1ba4
techno-thor <joejeakle@gmail.com>
2026-03-11 10:32:07 -0400

    DashboardValue updates

--- dashboard/DashboardValue.java
+++ dashboard/DashboardValue.java
@@ -1,5 +1,6 @@
 package frc.hawklib.dashboard;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -13,12 +14,16 @@ public class DashboardValue<ValueType> {
     private final NetworkTableEntry ENTRY;
 
     private final Supplier<ValueType> GETTER;
+    private final Consumer<ValueType> SETTER;
 
-    public DashboardValue(NetworkTable table, String title) { this(table, title, null); }
-    public DashboardValue(NetworkTable table, String title, Supplier<ValueType> getter) {
+    public DashboardValue(NetworkTable table, String title) { this(table, title, null, null); }
+    public DashboardValue(NetworkTable table, String title, Supplier<ValueType> getter) { this(table, title, getter, null); }
+    public DashboardValue(NetworkTable table, String title, Consumer<ValueType> setter) { this(table, title, null, setter); }
+    
+    public DashboardValue(NetworkTable table, String title, Supplier<ValueType> getter, Consumer<ValueType> setter) {
         ENTRY = table.getEntry(title); 
-        ENTRY.getTopic();
         GETTER = getter;
+        SETTER = setter;
 
         DashboardManager.add(this);
     }
@@ -28,6 +33,7 @@ public class DashboardValue<ValueType> {
 
     public void update() {
         if(GETTER != null) set(GETTER.get());
+        if(SETTER != null) SETTER.accept(get());
     }
 
     public void delete() { ENTRY.unpublish(); }
